3d9dc5d380b9adc032cb75c40d6b0f5a4cf360a7
Army Nougues <anougues@freepik.com>
2024-10-26 19:29:41 +0200

    add client react query

--- tRPC-Demo/trpclibrary/client-side/src/App.tsx
+++ tRPC-Demo/trpclibrary/client-side/src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+// Default Import Statements
 
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+
+// Add the following Import Statements
+
+import {useState} from "react";
+
+import {trpc} from "./trpc";
+import {httpBatchLink} from "@trpc/client";
+import AppComponent from "./AppComponent";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+import "./index.scss";
+
+const client = new QueryClient();
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
@@ -10,10 +24,10 @@ const App = () => (
     <div>Language: TypeScript</div>
     <div>CSS: Tailwind</div>
   </div>
-)
-const rootElement = document.getElementById('app')
-if (!rootElement) throw new Error('Failed to find the root element')
+);
+const rootElement = document.getElementById("app");
+if (!rootElement) throw new Error("Failed to find the root element");
 
-const root = ReactDOM.createRoot(rootElement as HTMLElement)
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
 
-root.render(<App />)
+root.render(<App />);
