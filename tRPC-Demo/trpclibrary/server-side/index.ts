295d539dee16b58e81593b3a18ba2410fb8715c0
Army Nougues <anougues@freepik.com>
2024-10-26 19:19:03 +0200

    add hello tRPC endpoint

--- tRPC-Demo/trpclibrary/server-side/index.ts
+++ tRPC-Demo/trpclibrary/server-side/index.ts
@@ -2,14 +2,41 @@ import express from "express";
 import {initTRPC} from "@trpc/server";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import zod from "zod";
+import cors from "cors";
 
 const createContext = ({}: trpcExpress.CreateExpressContextOptions) => ({});
 type Context = Awaited<ReturnType<typeof createContext>>;
 
 const trpc = initTRPC.context<Context>().create();
 
+const appRouter = trpc.router({
+  hello: trpc.procedure
+    .input(
+      zod.object({
+        name: zod.string(),
+      })
+    )
+    .query(({input}) => {
+      return {
+        name: input.name,
+      };
+    }),
+});
+
+export type AppRouter = typeof appRouter;
+
 const app = express();
 const port = 3005;
+
+app.use(cors());
+
+app.use(
+  "/api",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello from server-side");
